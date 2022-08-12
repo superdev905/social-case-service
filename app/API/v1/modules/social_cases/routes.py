e049412e9a7858b504af8c1dfcc76a63d1728fda
superdev905 <superdev905@gmail.com>
2022-08-12 00:59:04 -0400

    Cambio de correos

--- app/API/v1/modules/social_cases/routes.py
+++ app/API/v1/modules/social_cases/routes.py
@@ -334,8 +334,7 @@ def send_social_case_mail(type: str, body: SocialCaseMail = None):
     #Variables:
     # type: Tipo de mensaje a enviar (CREATE, EDIT, ASIGN). <-- Param
     # body: Data general que va en cada cuerpo del correo y a quién va dirigido en variable body.to <-- Body
-    # "ruben.kern@itprocesos.cl", "ecarrizo@fundacioncchc.cl" <-- Agregar a arreglo para pruebas de mañana.
-    to = ["jonathan.diaz@itprocesos.cl", "eduardo.molina@itprocesos.cl"]
+    to = ["jonathan.diaz@itprocesos.cl", "eduardo.molina@itprocesos.cl", "ruben.kern@itprocesos.cl", "ecarrizo@fundacioncchc.cl"]
     # create message object instance
     msg = MIMEMultipart('alternative')
     # setup the parameters of the message
