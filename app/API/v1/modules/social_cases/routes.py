38764dbd491c2b2bb07526de14dfe3ad1169c182
superdev905 <superdev905@gmail.com>
2022-08-15 21:46:22 -0400

    Fix general

--- app/API/v1/modules/social_cases/routes.py
+++ app/API/v1/modules/social_cases/routes.py
@@ -334,7 +334,6 @@ def send_social_case_mail(type: str, body: SocialCaseMail = None):
     #Variables:
     # type: Tipo de mensaje a enviar (CREATE, EDIT, ASIGN). <-- Param
     # body: Data general que va en cada cuerpo del correo y a quién va dirigido en variable body.to <-- Body
-    # to = ["jonathan.diaz@itprocesos.cl", "eduardo.molina@itprocesos.cl", "ruben.kern@itprocesos.cl", "ecarrizo@fundacioncchc.cl"]
     # create message object instance
     msg = MIMEMultipart('alternative')
     # setup the parameters of the message
