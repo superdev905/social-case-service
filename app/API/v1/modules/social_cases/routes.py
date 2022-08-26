b1cbd54e46091603e73a933034f18e2dfd55f757
superdev905 <superdev905@gmail.com>
2022-08-26 16:12:41 -0400

    Fix filtros

--- app/API/v1/modules/social_cases/routes.py
+++ app/API/v1/modules/social_cases/routes.py
@@ -36,7 +36,7 @@ def get_all(business_id: int = Query(None, alias="businessId"),
             end_date: Optional[datetime] = Query(None, alias="endDate"),
             zone: str = None,
             state: str = None,
-            asistance_id: int = Query(None, alias="asistanceId"),
+            assistance_id: int = Query(None, alias="assistanceId"),
             professional_id: int = Query(None, alias="professionalId"),
             delegation: str = None,
             area_id: int = Query(None, alias="areaId"),
@@ -54,9 +54,9 @@ def get_all(business_id: int = Query(None, alias="businessId"),
 
     if(business_id):
         filters.append(SocialCase.business_id == business_id)
-    if(asistance_id):
-        filters.append(SocialCase.professional_id == asistance_id)
-        extraFilters.append(SocialCase.assistance_derivation_id.contains([asistance_id]))
+    if(assistance_id):
+        filters.append(SocialCase.professional_id == assistance_id)
+        extraFilters.append(SocialCase.assistance_derivation_id.contains([assistance_id]))
     if(professional_id):
         filters.append(SocialCase.professional_id == professional_id)
         extraFilters.append(SocialCase.assistance_derivation_id.contains([professional_id]))
